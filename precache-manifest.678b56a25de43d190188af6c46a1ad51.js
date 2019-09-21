0f26e176b78c8147980e41dfdcb2743537c7b8f2
Gina <fogasgeorgina@gmail.com>
2019-09-21 22:54:33 +0300

    Updates

---
+++ precache-manifest.678b56a25de43d190188af6c46a1ad51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a12d529bc228ec3381f94196d8216e",
+    "url": "/picsalist/index.html"
+  },
+  {
+    "revision": "f478856ed83260a9d960",
+    "url": "/picsalist/static/css/main.cdf209ea.chunk.css"
+  },
+  {
+    "revision": "1826e62e1e14564d436d",
+    "url": "/picsalist/static/js/2.da65d401.chunk.js"
+  },
+  {
+    "revision": "f478856ed83260a9d960",
+    "url": "/picsalist/static/js/main.46ab1fb9.chunk.js"
+  },
+  {
+    "revision": "8d045365411ae3027807",
+    "url": "/picsalist/static/js/runtime~main.a68c9adb.js"
+  }
+]);
